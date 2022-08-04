25d082b04eaabf3ca68f95c8b015a04ad5a24590
Seej <seej@bposeats.com>
2022-08-04 18:39:30 +0800

    days 7 nto 9, imperfect

---
+++ 007/day7.js
@@ -0,0 +1,8 @@
+// $('.icon-search').bind('click', function() {
+//     $('.search').toggleClass('active');
+// });
+
+// $('.icon-nav').bind('click', function() {
+//     $('.panel').toggleClass('show-menu');
+//     $('.menu').toggleClass('active');
+// });
